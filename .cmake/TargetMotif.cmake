537328453c4ad2eeee19c4be73bcfe0f37249b76
Ryan Burns <rtburns-jpl@users.noreply.github.com>
2020-07-04 17:00:10 -0700

    Link to x11 libs transitively via motif

--- .cmake/TargetMotif.cmake
+++ .cmake/TargetMotif.cmake
@@ -1,9 +1,14 @@
-find_package(Motif)
+if(TARGET X11::Xt)
 
-if(MOTIF_FOUND AND NOT TARGET Motif::Motif)
-    add_library(Motif::Motif IMPORTED INTERFACE)
-    target_include_directories(Motif::Motif
-        SYSTEM INTERFACE ${MOTIF_INCLUDE_DIR})
-    target_link_libraries(Motif::Motif
-        INTERFACE ${MOTIF_LIBRARIES})
+    find_package(Motif)
+
+    if(MOTIF_FOUND AND NOT TARGET Motif::Motif)
+        add_library(Motif::Motif IMPORTED INTERFACE)
+        target_include_directories(Motif::Motif
+            SYSTEM INTERFACE ${MOTIF_INCLUDE_DIR})
+        target_link_libraries(Motif::Motif
+            INTERFACE ${MOTIF_LIBRARIES}
+                      X11::Xt
+            )
+    endif()
 endif()
